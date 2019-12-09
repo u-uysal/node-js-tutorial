5219e369b7db224d3b472cc8dfafbb21c3f183d3
ufuk <ufuk.uysl5@gmail.com>
2019-12-09 15:10:11 +0100

    nodemon install and basic example

--- tutorial-1/section-4/app.js
+++ tutorial-1/section-4/app.js
@@ -3,11 +3,11 @@ var http = require('http'); // bir web sunucusu oluşturmak için kullanılan mo
 http.createServer(function (request, response) { //createServer web sunucusu oluşturma yöntemi
 // request : bir web sunucusuna yapılan istektir
 //response : isteğe karşılık serverın yaptığı yanıt
-  console.log(request.headers)
+  console.log("merhaba")
 
   response.writeHead(200, {'Content-Type': 'text/html ; charset = utf-8 '});
   // 200 ün manası yapılan istek ok! .gayet legal istek demek
   // aynı zamanda cevabın hangi formatta gönderileceği content type ile belli edilir
 
-  response.end('Hello World!!!');// kullanıcıya verilecek cevap
+  response.end('Hello new World!!');// kullanıcıya verilecek cevap
 }).listen(8080); // bu cevabı nerde yayınlayacaksınızın cevap
