a45c2593890d2cc6ee1ce9253f55c2748bf83d0b
ufuk <ufuk.uysl5@gmail.com>
2019-12-09 15:42:30 +0100

    how to install a package and use.

---
+++ tutorial-1/section-5/app.js
@@ -0,0 +1,12 @@
+const slugify = require('slugify'); // modulu dahil etmek
+
+const texting = "how to install a module.";
+
+const slug = slugify(texting); // some-string
+
+console.log(slug); //how-to-install-a-module.
+
+const slug2 = slugify(texting, "*"); // some-string
+
+console.log(slug2) //how*to*install*a*module.
+
