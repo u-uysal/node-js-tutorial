4c263b5e56c8878128b5b6b24941158c4f5c1703
ufuk <ufuk.uysl5@gmail.com>
2019-12-11 16:32:58 +0100

    create and remove dir .

---
+++ ninja/create-romove directory/app.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+//https://www.yusufsezer.com.tr/node-js-fs/
+
+// sync version
+
+//fs.mkdirSync("application"); // creating directory
+
+//fs.rmdirSync("application"); // remove directory
+
+
+
+
+
+
+
+//async version
+
+
+
+//creating
+
+/* fs.mkdir('hello', function () {
+
+  console.log('success');
+
+}); */
+
+
+// removing
+
+/* fs.rmdir('hello', function () {
+
+  console.log('success');
+
+}); */
